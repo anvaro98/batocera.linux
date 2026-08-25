b808b6c3f8212dd2b6582eb0df55dfb9fbd05f06
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2026-08-25 20:41:32 +0000

    feat: configure labwc screens via configgen
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/melonds/melondsGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/melonds/melondsGenerator.py
@@ -234,6 +234,11 @@ class MelonDSGenerator(Generator):
         with configFileName.open("w") as toml_file:
             toml.dump(config, toml_file)
 
+        # windows position is handled by coordonnates by the application (xorg) or the windows manager (wayland)
+        screens = videoMode.getScreensInfos(system.config)
+        videoMode.configureWindow(screens, identifier="net.kuribo64.melonDS", title="*w1*", output="primary")
+        videoMode.configureWindow(screens, identifier="net.kuribo64.melonDS", title="*w2*", output="backglass")
+
         commandArray = ["/usr/bin/melonDS", "-f", rom]
         return Command.Command(
             array=commandArray,
